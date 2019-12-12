c8d3d1ad100aa8cee0045eea0971445444b9e568
Rajesh Sharma <broncha@rajesharma.com>
2019-12-12 22:54:15 +0545

    add file uploader

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,6 +28,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter";
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -40,6 +41,7 @@ ClassicEditor.builtinPlugins = [
 	Italic,
 	BlockQuote,
 	CKFinder,
+    SimpleUploadAdapter,
 	EasyImage,
 	Heading,
 	Image,
